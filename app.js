8688e9f13e54edaa8249d889b6336de4d4da1dc5
Grace2me <145139703+Grace2me@users.noreply.github.com>
2023-09-19 09:55:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_cczcb0i", "template_k7ixms9", formData)
+    emailjs.send("service_k5xq1uf", "template_56yzgx8", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
